4e9b42966222b46799864a35fb2e430e4d9145d8
Micah Lessnick <micahlessnick@gmail.com>
2021-11-16 15:57:40 -0500

    Added functionality to buttons and began adding logic. Right now, if user selects 2 colors, userColor is set to the combination as ID'd by the inputsToOutputs dictionary. Added flags and logic for switching between normal and hardmode (have not extensively tested this). userColor is updated after every other button is pressed. Added randomizer for target color and connected label to storyboard

--- CC_Lessnick/CC_Lessnick/ViewController.swift
+++ CC_Lessnick/CC_Lessnick/ViewController.swift
@@ -12,50 +12,100 @@ class ViewController: UIViewController {
     @IBOutlet weak var cannonBallImage: UIImageView!
     
     var firstColorPicked: Bool = false
-    //flag indicating whether or not the first color has been picked
+    var hardMode: Bool = false
+    //flags for hard mode and number of selections
     
     var firstColor: String = ""
-    //string containing the name of the first color the user picked
     var secondColor: String = ""
+    //user's selection
     
-    var normalColors = ["Blue", "Red", "Yellow"]
+    var normalColors = ["Red", "Blue", "Yellow"]
+    var normalTargets = ["Red", "Blue", "Yellow", "Purple", "Green", "Orange"]
     var hardColors = ["Cyan", "Magenta", "Yellow"]
+    var hardTargets = ["Cyan", "Magenta", "Yellow", "Blue", "Red", "Green"]
+    var colorArray:[String] = []
+    var targetArray:[String] = []
     //array containing available colors to be chosen in each game mode
+    
+    var targetColor = ""
+    var userColor = ""
+    //target and user input
+    
+    
+    let inputsToOutput = ["RedRed":"Red",
+                          "RedBlue":"Purple",
+                          "RedYellow":"Orange",
+                          "BlueBlue":"Blue",
+                          "BlueYellow":"Green",
+                          "BlueRed":"Purple",
+                          "YellowYellow":"Yellow",
+                          "YellowRed":"Orange",
+                          "YellowBlue":"Green",
+                          "CyanCyan":"Cyan",
+                          "CyanMagenta":"Blue",
+                          "CyanYellow":"Green",
+                          "MagentaMagenta":"Magenta",
+                          "MagentaYellow":"Red",
+                          "MagentaCyan":"Blue",
+                          "YellowCyan":"Green",
+                          "YellowMagenta":"Red"]
+    //dictionary containing every possible user input + resulting output
+    
+    @IBOutlet weak var top: UIButton!
+    @IBOutlet weak var mid: UIButton!
+    @IBOutlet weak var bot: UIButton!
+
+    @IBOutlet weak var colorDisplay: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if hardMode{
+            //switch available color array based on mode selection
+            //allows fewer if statements in switch
+            colorArray = hardColors
+            targetArray = hardTargets
+        }
+        else{
+            colorArray = normalColors
+            targetArray = normalTargets
+        }
+        
+        targetColor = targetArray.randomElement()!
+        colorDisplay.text = targetColor
     }
 
     @IBAction func paintCanClicked(_ sender: UIButton) {
         if !firstColorPicked{
             //first color has not been picked yet, the user has just selected it
             //set firstColor to the first color and change cannon ball
+            switch sender {
+            case top:
+                firstColor = colorArray[0]
+                //add code to highlight selection (?)
+            case mid:
+                firstColor = colorArray[1]
+            default:
+                firstColor = colorArray[2]
+            }
+            firstColorPicked.toggle()
         }
         else{
             //fully fill cannon ball and enable shoot button
             //set second color to color picked by user
-            switch firstColor {
-            case "Blue":
-                switch <#value#> {
-                case <#pattern#>:
-                    <#code#>
-                default:
-                    <#code#>
-                }
-            case "Red":
-                switch <#value#> {
-                case <#pattern#>:
-                    <#code#>
-                default:
-                    <#code#>
-                }
-            
+            switch sender {
+            case top:
+                secondColor = colorArray[0]
+            case mid:
+                secondColor = colorArray[1]
             default:
-                //default at the moment will be yellow. More cases will be added for "hard mode"
-                <#code#>
+                secondColor = colorArray[2]
             }
+            userColor = inputsToOutput[firstColor+secondColor]!
+            firstColorPicked.toggle()
+            
         }
     }
     
